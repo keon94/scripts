3240b1906afc913323f35253853f102eccdd1ad3
Keon Amini <Keon.Amini@cerner.com>
2020-09-29 16:35:39 -0500

    text finder added

--- intellij_logs.sh
+++ intellij_logs.sh
@@ -7,9 +7,9 @@ INTELLIJ_LOGS_DIR="$HOME/AppData/Local/JetBrains/IdeaIC2020.2/log/"
 cd "$INTELLIJ_LOGS_DIR"
 
 if [ $1 == "idea" ]; then
-    tail -f -n 1000 "idea.log"
+    tail --follow=name -n 1000 "idea.log"
 elif [ $1 == "DEBUG" ]; then
-    tail -f -n 1000 "build-log/build.log" 
+    tail --follow=name -n 1000 "build-log/build.log" 
 else
-    tail -f -n 1000 "build-log/build.log" | grep -v "DEBUG"
+    tail --follow=name -n 1000 "build-log/build.log" | grep -v "DEBUG"
 fi
