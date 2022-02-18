2787c93a4336281f5743e3b8ac8b803c8537424b
Keon Amini <keon.amini@skael.com>
2022-02-18 17:34:08 -0600

    uplifted intellij logs

--- intellij_logs.sh
+++ intellij_logs.sh
@@ -2,13 +2,17 @@
 
 #/bin/sh
 
-INTELLIJ_LOGS_DIR="$HOME/AppData/Local/JetBrains/IdeaIC2020.2/log/"
+INTELLIJ_LOGS_DIR="$HOME/.cache/JetBrains/IntelliJIdea2021.3/log"
 
 cd "$INTELLIJ_LOGS_DIR"
 
-if [ $1 == "idea" ]; then
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+if [[ $1 == "idea" ]]; then
     tail --follow=name -n 1000 "idea.log"
-elif [ $1 == "DEBUG" ]; then
+elif [[ $1 == "DEBUG" ]]; then
     tail --follow=name -n 1000 "build-log/build.log" 
 else
     tail --follow=name -n 1000 "build-log/build.log" | grep -v "DEBUG"
